29a7c9684817199668d882876dada57a7cf8eb55
kyungbbabi <kyung981124@naver.com>
2022-04-24 00:04:30 +0900

    로그인 페이지 컴포턴트 구성

--- src/LoginPage/Login.js
+++ src/LoginPage/Login.js
@@ -1,68 +1,88 @@
 import React, { Component } from 'react';
-import styled from 'styled-components';
+
 import oc from 'open-color';
 import styles from './Login.css'
+import AppBar from '@mui/material/AppBar';
+import InputBase from '@mui/material/InputBase';
+import { styled, alpha } from '@mui/material/styles';
+import SearchIcon from '@mui/icons-material/Search';
+import Typography from '@mui/material/Typography';
 
-const Label = styled.div`
-    width: 100%;
-    border: 1px solid ${oc.gray[3]};
-    outline: none;
-    border-radius: 0px;
-    line-height: 2.5rem;
-    font-size: 1.2rem;
-    padding-left: 0.5rem;
-    padding-right: 0.5rem;
-    ::placeholder {
-    color: ${oc.gray[3]};
-    }
-`;
+import Toolbar from '@mui/material/Toolbar';
 
-const Input = styled.input`
-    width: 100%;
-    border: 1px solid ${oc.gray[3]};
-    outline: none;
-    border-radius: 0px;
-    line-height: 2.5rem;
-    font-size: 1.2rem;
-    padding-left: 0.5rem;
-    padding-right: 0.5rem;
-`;
+import { Box } from '@material-ui/core';
+import { Avatar } from '@mui/material';
+import ButtonBase from '@mui/material/ButtonBase';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
 
-// rest 쪽에는 onChange, type, name, value, placeholder 등의 input 에서 사용 하는 값들을 넣어줄수 있다.
-const InputWithLabel = ({label, ...rest}) => (
-    <Wrapper>
-        <Label>{label}</Label>
-        <Input {...rest}/>
-    </Wrapper>
-);
+const LoginPageTool = styled('div')(({theme}) => ({
+    display: 'flex',
+    justifyContent:'center',
+    alignItems:'center',
+    margin: 'auto 0',
+	marginRight: '10px',
+	padding: 3,
+    width: "auto",
+	
+    backgroundColor: alpha(theme.palette.common.black, 0.15)
 
-const Title = styled.div`
-    font-size: 1.5rem;
-    font-weight: 500;
-    color: ${oc.gray[8]};
-    margin-bottom: 1rem;
-`;
+}))
 
-const AuthContent = ({title}) => (
-    <div>
-        <Title>{title}</Title>
-    </div>
-);
+// const Label = styled.div`
+//     width: 100%;
+//     border: 1px solid ${oc.gray[3]};
+//     outline: none;
+//     border-radius: 0px;
+//     line-height: 2.5rem;
+//     font-size: 1.2rem;
+//     padding-left: 0.5rem;
+//     padding-right: 0.5rem;
+//     ::placeholder {
+//     color: ${oc.gray[3]};
+//     }
+// `;
 
-const AuthButton = ({onClick}) => (
-    <Wrapper onClick={onClick}>
-    </Wrapper>
-);
-
-const Wrapper = styled('div') ( ({ }) => ({
-    marginTop: '1rem',
-    paddingTop: '0.6rem',
-    paddingBottom: '0.5rem',
-    background: oc.blue[6],
-    color: 'white',
-
-
+const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    [theme.breakpoints.up('sm')]: {
+      width: 'auto'
+    },
 }));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(3)})`,
+          width: '100%'
+    },
+}));
+
+const Title = styled(Typography)(() => ({
+	fontSize: '20px',
+	fontWeight: 'bold',
+}));
+
+// const AuthButton = ({onClick}) => (
+//     <Wrapper onClick={onClick}>
+//     </Wrapper>
+// );
+
+// const Wrapper = styled('div') ( ({ }) => ({
+//     marginTop: '1rem',
+//     paddingTop: '0.6rem',
+//     paddingBottom: '0.5rem',
+//     background: oc.blue[6],
+//     color: 'white',
+
+
+// }));
 class Login extends Component {
 
 
@@ -72,25 +92,26 @@ class Login extends Component {
         return (
             <>
             <div className={styles.background}>
-                <AuthButton></AuthButton>
-            <AuthContent title="로그인">
-                <InputWithLabel 
-                    label="이메일" 
-                    name="email" 
-                    placeholder="이메일" 
-                    
+                {/* <AuthButton></AuthButton> */}
+
+                {/* <InputWithLabel 
+                   placeholder="프로필 검색"
+                   inputProps={{ 'aria-label': 'search' }}
+                /> */}
+                <LoginPageTool>
+                <StyledInputBase
+                placeholder="아이디"
+							inputProps={{ 'aria-label': 'search' }}
                 />
-                <InputWithLabel 
-                    label="비밀번호" 
-                    name="password" 
-                    placeholder="비밀번호" 
-                    type="password" 
-                    
+                <StyledInputBase
+                placeholder="패스워드"
+							inputProps={{ 'aria-label': 'search' }}
                 />
-                <AuthButton>로그인</AuthButton>
+                </LoginPageTool>
+                {/* <AuthButton>로그인</AuthButton> */}
                 {/* 아이디/비밀번호를 잊어버렸어요.
                 회원가입 */}
-            </AuthContent>
+          
             </div>
             </>
         );
