05ec66f4d55d8615792f3cb1deda23e5a97f77f0
unknown <eodrmfdl1004@naver.com>
2022-09-29 02:19:34 +0900

    회원가입 이메일 인증

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 
-import Typography from '@mui/material/Typography';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
-import { Box, Button, Dialog, DialogContent, Grid, Stack, TextField } from '@mui/material';
+import { Box, Button, Dialog, DialogContent, Grid, Stack, TextField, Typography } from '@mui/material';
 import Register from './Register';
 import './anchorCss.css';
 import axios from 'axios';
@@ -10,16 +9,20 @@ import axios from 'axios';
 export default function Login(props) {
 
   const [open, setOpen] = useState(false);
-  const [loginInfo, setLoginInfo] = useState({id: null, password: null});
+  const [mailAuth, setMainAuth] = useState(false);
+  const [loginInfo, setLoginInfo] = useState({uid: null, password: null});
+  const [mail, setMail] = useState(null);
   const [errorMessage, setErrorMessage] = useState(null);
   const [idErrorMessage, setIdErrorMessage] = useState(null);
   const [pwErrorMessage, setPwErrorMessage] = useState(null);
+  const [codeErrorMessage, setCodeErrorMessage] = useState(null);
 
-  const handleClickOpen = () => { setOpen(true); }
-  const handleClickClose = () => { setOpen(false); }
+  const handleClickOpen = () => { setOpen(true) };
+  const handleClickClose = () => { setOpen(false) };
   const handleClickLogin = (e) => {
     e.preventDefault();
     props.getLogin(true);
+    console.log(loginInfo)
   }
   const handleValueChange = (e) => {
     const {name, value} = e.target;
@@ -30,41 +33,55 @@ export default function Login(props) {
   }
 
   return (
-    <>
-      <Stack sx={{ width: 300, position: 'absolute', transform:'translate(-50%, -50%)', 
-        left: '50%', top: '50%', borderRadius: '30px', padding: '60px', boxShadow: 6 }} 
-        spacing={2} align='center'>
-        <Grid align='center'>
-          <MenuBookIcon color='primary' sx={{ fontSize: 100 }}/>
-          <Typography variant='h4' color='primary' fontWeight='bold'>모두의 일기장</Typography>
-        </Grid>
-        <Box component="form">
-          <Stack spacing={1}>
-            <TextField label='ID' name="id" onChange={handleValueChange}
-              helperText={idErrorMessage} error={idErrorMessage ? true : false}
+      <>
+        <Stack sx={{
+          width: 300, position: 'absolute', transform: 'translate(-50%, -50%)',
+          left: '50%', top: '50%', borderRadius: '30px', padding: '60px', boxShadow: 6
+        }}
+               spacing={2} align='center'>
+          <Grid align='center'>
+            <MenuBookIcon color='primary' sx={{fontSize: 100}}/>
+            <Typography variant='h4' color='primary' fontWeight='bold'>모두의 일기장</Typography>
+          </Grid>
+
+          <Box component="form">
+            <Stack spacing={1}>
+              <TextField label='ID' name="uid" onChange={handleValueChange}
+                         helperText={idErrorMessage} error={!!idErrorMessage}
+              />
+              <TextField label='PW' type='password' name="password" onChange={handleValueChange}
+                         helperText={pwErrorMessage} error={!!pwErrorMessage}
+              />
+              {mailAuth &&
+                  <>
+                    <TextField label='Email' type='mail' name="mail" onChange={handleValueChange}/>
+                    <TextField label='인증코드' name="code" onChange={handleValueChange}
+                               helperText={codeErrorMessage} error={!!codeErrorMessage}
+                    />
+                  </>
+              }
+
+              <Button type='submit' variant='contained' color='success' onClick={handleClickLogin}>로그인</Button>
+              <Typography color='red' fontSize={12}>{errorMessage}</Typography>
+            </Stack>
+          </Box>
+
+          <a href="/">아이디/비밀번호를 잊어버렸어요</a>
+          <Typography variant='subtitle2' color="text.secondary">또는</Typography>
+          <Button onClick={handleClickOpen} variant='contained'>회원가입</Button>
+        </Stack>
+
+        <Dialog open={open} onClose={handleClickClose} maxWidth='xs' fullWidth={true}>
+          <DialogContent>
+            <Register
+                getLogin={props.getLogin}
+                toggleLoading={props.toggleLoading}
+                setName={props.setName}
+                setEmail={props.setEmail}
+                setId={props.setId}
             />
-            <TextField label='PW' type='password' name="password" onChange={handleValueChange}
-              helperText={pwErrorMessage} error={pwErrorMessage ? true : false}
-            />
-           <Button type='submit' variant='contained' color='success' onClick={handleClickLogin}>로그인</Button>
-           <Typography color='red' fontSize={12}>{errorMessage}</Typography>
-          </Stack>
-        </Box>
-        <a href="/">아이디/비밀번호를 잊어버렸어요</a>
-        <Typography variant='subtitle2' color="text.secondary">또는</Typography>
-        <Button onClick={handleClickOpen} variant='contained'>회원가입</Button>  
-      </Stack>
-      <Dialog open={open} onClose={handleClickClose} maxWidth='xs' fullWidth={true}>
-        <DialogContent>
-          <Register 
-            getLogin={props.getLogin} 
-            toggleLoading={props.toggleLoading}
-            setName={props.setName}
-            setEmail={props.setEmail}
-            setId={props.setId}
-          />
-        </DialogContent>
-      </Dialog>
-    </>
+          </DialogContent>
+        </Dialog>
+      </>
   );
 }
--- src/components/login/Register.js
+++ src/components/login/Register.js
@@ -1,30 +1,73 @@
 import React, { useState } from 'react';
-import Typography from '@mui/material/Typography';
 import MenuBookIcon from '@mui/icons-material/MenuBook';
-import { Box, Button, Grid, Stack, TextField } from '@mui/material';
-import axios from 'axios';
+import { Box, Button, Grid, Stack, TextField, Typography } from '@mui/material';
+import {authApi} from "../../server/auth";
 
 export default function Register(props) {
-
+	const [mode, setMode] = useState('AUTH');
+	const [auth, setAuth] = useState(false);
+	const [authInfo, setAuthInfo] = useState({code: null, email: null})
 	const [registInfo, setRegistInfo] = useState({
-		id: null,
+		uid: null,
 		password: null,
 		passwordCheck: null,
-		name: null,
-		email: null
+		name: null
 	});
-	const [idErrorMessage, setIdErrorMessage] = useState(null);
-	const [pwErrorMessage, setPwErrorMessage] = useState(null);
-	const [pwchkErrorMessage, setPwchkErrorMessage] = useState(null);
-	const [nameErrorMessage, setNameErrorMessage] = useState(null);
-	const [emailErrorMessage, setEmailErrorMessage] = useState(null);
+	const [idErrorMessage, setIdErrorMessage] = useState('');
+	const [pwErrorMessage, setPwErrorMessage] = useState('');
+	const [pwchkErrorMessage, setPwchkErrorMessage] = useState('');
+	const [nameErrorMessage, setNameErrorMessage] = useState('');
+	const [mailErrorMessage, setMailErrorMessage] = useState('');
+	const [codeErrorMessage, setCodeErrorMessage] = useState('');
+
+	const mailValidate = () => {
+		let valid = true;
+		let regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+		if (regEmail.test(authInfo.email) === false || !authInfo.email) {
+			setMailErrorMessage('메일 주소를 올바르게 입력해 주세요.');
+			valid = false;
+		} else setMailErrorMessage('');
+		return valid;
+	};
+	const authValidate = () => {
+		let valid = true;
+		let regEmail = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+		if (regEmail.test(authInfo.email) === false || !authInfo.email) {
+			setMailErrorMessage('메일 주소를 올바르게 입력해 주세요.');
+			valid = false;
+		} else setMailErrorMessage('');
+		if (!authInfo.code) {
+			setCodeErrorMessage('인증 코드를 입력해 주세요.');
+			valid = false;
+		} else setCodeErrorMessage('');
+		return valid;
+	};
+
 	const handleValueChange = (e) => {
-    const {name, value} = e.target;
-    setRegistInfo({
-      ...registInfo,
-      [name]: value
-    });
-  }
+		const {name, value} = e.target;
+		setRegistInfo({
+			...registInfo,
+			[name]: value
+		});
+	};
+	const handleAuthInfoChange = (e) => {
+		const {name, value} = e.target;
+		setAuthInfo({
+			...authInfo,
+			[name]: value
+		});
+	};
+	const handleClickCodeCheck = (e) => {
+		e.preventDefault();
+		authValidate() &&
+		authApi.checkEmailAuthMail(authInfo)
+			.then(res => setMode('REGIST'))
+			.catch(error => setCodeErrorMessage('인증 코드가 일치하지 않습니다.'))
+	};
+	const handleSendMail = (e) => {
+		e.preventDefault();
+		mailValidate() && authApi.authenticationEmail(authInfo.email, setAuth(true))
+	};
 	const handleClickRegist = (e) => {
 		e.preventDefault();
 		props.getLogin(true);
@@ -37,27 +80,48 @@ export default function Register(props) {
 				<Typography variant='h4' color='primary' fontWeight='bold'>모두의 일기장</Typography>
 			</Grid>
 
-			<Box component="form">
-				<Stack spacing={1}>
-					<TextField fullWidth label='ID' onChange={handleValueChange} name="id"
-						error={idErrorMessage ? true : false} helperText={idErrorMessage} />
+			{mode === 'AUTH' &&
+				<Box component="form">
+					<Stack spacing={1}>
+						<TextField fullWidth label='Email' onChange={handleAuthInfoChange} name="email"
+											 error={!!mailErrorMessage} helperText={mailErrorMessage}
+						/>
+						<Box sx={{display: auth ? 'block' : 'none', textAlign: 'end'}}>
+						<Button onClick={handleSendMail} size='small' sx={{width: 'max-content'}}>인증코드 재전송</Button>
+						</Box>
 
-					<TextField fullWidth label='PW' type='password' onChange={handleValueChange} name="password"
-						error={pwErrorMessage ? true : false} helperText={pwErrorMessage} />
+						<TextField fullWidth label='인증코드' onChange={handleAuthInfoChange} name="code"
+											 error={!!codeErrorMessage} helperText={codeErrorMessage}
+											 sx={{display: auth ? 'block' : 'none'}}
+						/>
+						<Button type='submit' variant='contained' color='primary' onClick={auth ? handleClickCodeCheck : handleSendMail}>
+							{auth ? "확인" : "인증코드 발송"}
+						</Button>
+					</Stack>
+				</Box>
+			}
 
-					<TextField fullWidth label='PW 확인' type='password' onChange={handleValueChange} name="passwordCheck"
-						error={pwchkErrorMessage ? true : false} helperText={pwchkErrorMessage} />
+			{mode === 'REGIST' &&
+				<Box component="form">
+					<Stack spacing={1}>
+						<TextField fullWidth label='ID' onChange={handleValueChange} name="uid"
+											 error={!!idErrorMessage} helperText={idErrorMessage}
+						/>
+						<TextField fullWidth label='PW' type='password' onChange={handleValueChange} name="password"
+											 error={!!pwErrorMessage} helperText={pwErrorMessage}
+						/>
+						<TextField fullWidth label='PW 확인' type='password' onChange={handleValueChange} name="passwordCheck"
+											 error={!!pwchkErrorMessage} helperText={pwchkErrorMessage}
+						/>
+						<TextField fullWidth label='이름' onChange={handleValueChange} name="name"
+											 error={!!nameErrorMessage} helperText={nameErrorMessage}
+						/>
 
-					<TextField fullWidth label='이름' onChange={handleValueChange} name="name"
-						error={nameErrorMessage ? true : false} helperText={nameErrorMessage} />
-
-					<TextField fullWidth label='Email' onChange={handleValueChange} name="email"
-						error={emailErrorMessage ? true : false} helperText={emailErrorMessage} />
-
-					<Button type='submit' variant='contained' color='primary'
-						onClick={handleClickRegist}>회원가입</Button>
-				</Stack>
-			</Box>
+						<Button type='submit' variant='contained' color='primary'
+										onClick={handleClickRegist}>회원가입</Button>
+					</Stack>
+				</Box>
+			}
 		</Stack>
   )
 }
