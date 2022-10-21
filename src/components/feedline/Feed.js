71f41595c67d79c855c2baf0dc36821e3d53e0df
unknown <eodrmfdl1004@naver.com>
2022-10-21 17:00:05 +0900

    feat: 루트 댓글 조회, 작성

--- src/components/feedline/Feed.js
+++ src/components/feedline/Feed.js
@@ -34,21 +34,6 @@ export default function Feed(props) {
   const [state, dispatch] = useContext(store);
   const {commentCount, id, isLiked, likeCount, writer, content, createTime} = props.feed;
   const [feedDetail, setFeedDetail] = useState(null);
-
-  const comments = [
-    {
-      "comment_idx": 1,
-      "name": "유저1",
-      "image": "https://placeimg.com/100/100/people/1",
-      "content": "그들은 그들은 꽃 투명하되 인생을 위하여 힘있다. 우리 동력은 천지는 얼마나 황금시대를 봄바람이다. 원대하고, 있음으로써 못할 않는 것이다. 물방아 오직 타오르고 위하여서.그들은 그들은 꽃 투명하되 인생을 위하여 힘있다. 우리 동력은 천지는 얼마나 황금시대를 봄바람이다. 원대하고, 있음으로써 못할 않는 것이다. 물방아 오직 타오르고 위하여서.그들은 그들은 꽃 투명하되 인생을 위하여 힘있다. 우리 동력은 천지는 얼마나 황금시대를 봄바람이다. 원대하고, 있음으로써 못할 않는 것이다. 물방아 오직 타오르고 위하여서."
-    },
-    {
-      "comment_idx": 2,
-      "name": "유저2",
-      "image": "https://placeimg.com/100/100/people/2",
-      "content": "그들의 있는 불어 산야에 뜨거운지라, 피가 있는 구할 속잎나고, 사막이다. 끓는 고동을 내는 우리 황금시대의 위하여서. 이상은 눈이 청춘의 사막이다. 그들의 기쁘며, 얼마나 불어 광야에서 그들에게 있을 말이다. 그 말을 듣는 것 조차 하지 못할 것인 사람이다."
-    }
-  ];
   const [open, setOpen] = useState(false);
   const [anchor, setAnchor] = useState(null);
 
@@ -147,7 +132,6 @@ export default function Feed(props) {
         <DialogContent>
           <FeedDetail
             feedDetail={feedDetail}
-            comments={comments}
             feedList={props.feedList}
             getFeedDetail={getFeedDetail}
             closeContent={closeContent}
