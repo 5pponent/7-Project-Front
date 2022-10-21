f8b795c8baceed962a86848993b24e788b7b90c5
unknown <eodrmfdl1004@naver.com>
2022-10-21 13:37:24 +0900

    feat: 피드 이미지 설명 추가

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -27,7 +27,6 @@ const StyledInputBase = styled(InputBase)(({theme}) => ({
   color: 'inherit',
   '& .MuiInputBase-input': {
     padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(3)})`,
     width: '100%'
   },
